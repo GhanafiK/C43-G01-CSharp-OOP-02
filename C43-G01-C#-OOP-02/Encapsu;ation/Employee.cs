58bd9d495be5b8993d05d3400dc383b6621e05a3
GHK <gamalhanafi26@gmail.com>
2024-12-30 04:41:25 +0200

    Struct-Setter&Getter-Property(full - automatic)

---
+++ C43-G01-C#-OOP-02/Encapsu;ation/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C43_G01_C__OOP_02.Encapsu_ation
+{
+    internal struct Employee
+    {
+        #region Property
+        public int Id;
+        string Name;//use setter and getter with it
+        public int Age { get; set; }//use automatic property
+        decimal salary; // use full property 
+        #endregion
+
+        #region Constructor
+        public Employee(int id, string name, int age, decimal salary)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Salary = salary; //to check if value in constructor > 5000 set it 5000 otherwise set it with it's value
+        }
+        #endregion
+
+        #region Methods
+        public void setName(string name)
+        {
+            Name = name.Length > 5 ? name.Substring(0, 5) : name;
+
+        }
+        public string getName() { return Name; }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            private set
+            {
+                salary = value > 5000 ? 5000 : value;
+            }
+        }
+
+        //public decimal deduction; // deriven attribute 
+        public decimal Deduction
+        {
+            get { return salary - salary * .25m; }
+        }
+
+        public override string ToString()
+        {
+            return $"id: {Id} - name: {Name} - age: {Age} - salary:{salary}";
+        } 
+        #endregion
+    }
+}
--- C43-G01-C#-OOP-02/Program.cs
+++ C43-G01-C#-OOP-02/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace C43_G01_C__OOP_02
+﻿using C43_G01_C__OOP_02.Encapsu_ation;
+
+namespace C43_G01_C__OOP_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Demo
+            #region Struct - Setter Getter
+
+            Employee emp = new Employee(10, "Gamal", 22, 7000);
+            emp.Id = 1000;
+            Console.WriteLine(emp);
+            //it's wrong before encapsulation to access attribute directly and
+            //if we change in thing in struct any instance deal with this struct will be affected 
+            //we can change it's value 
+            //we can't add validation on it
+            // emp.name ->> it will be error because name is private so we can't deal with attribute directly
+            emp.setName("Gg");
+            Console.WriteLine($"new name: {emp.getName()}");
+
+            #endregion
+
+            #region Property
+
+            #region automatic property
+            emp.Age = 23;
+            Console.WriteLine($"new age: {emp.Age}");
+            #endregion
+
+            #region full property
+            //Deal with property like attribute but it property it creat a hidden attribute called backfield 
+            // it created by compiler in il (assymble code) 
+            Console.WriteLine($"Salary: {emp.Salary}");    
+            #endregion
+
+            #endregion
+
+            #endregion
+
+
         }
     }
 }
