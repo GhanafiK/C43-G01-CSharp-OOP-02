ee39774af7ebea2e7cb2101da188daa0d49a298b
GHK <gamalhanafi26@gmail.com>
2024-12-30 05:18:28 +0200

    PhoneBook(Setter - Getter)

---
+++ C43-G01-C#-OOP-02/Encapsu;ation/phoneBook.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C43_G01_C__OOP_02.Encapsu_ation
+{
+    internal struct phoneBook
+    {
+        #region attributes
+        string[] names;
+        int[] numbers;
+        int size;
+        #endregion
+
+        #region Constructor
+        public phoneBook(int size)
+        {
+            names = new string[size];
+            numbers = new int[size];
+            this.size = size;
+        }
+        #endregion
+
+        #region Methods
+        public void addPerson(int position, string name, int num)
+        {
+            if (names != null && numbers != null && position < size)
+            {
+                names[position] = name;
+                numbers[position] = num;
+            }
+        }
+
+        public int GetNumber(string name)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] == name)
+                {
+                    return numbers[i]; ;
+                }
+            }
+            return -1;
+        }
+
+        public void setNumber(int num, string name)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] == name)
+                {
+                    numbers[i] = num;
+                    break;
+                }
+            }
+        } 
+        #endregion
+    }
+}
--- C43-G01-C#-OOP-02/Program.cs
+++ C43-G01-C#-OOP-02/Program.cs
@@ -9,34 +9,57 @@ namespace C43_G01_C__OOP_02
             #region Demo
             #region Struct - Setter Getter
 
-            Employee emp = new Employee(10, "Gamal", 22, 7000);
-            emp.Id = 1000;
-            Console.WriteLine(emp);
-            //it's wrong before encapsulation to access attribute directly and
-            //if we change in thing in struct any instance deal with this struct will be affected 
-            //we can change it's value 
-            //we can't add validation on it
-            // emp.name ->> it will be error because name is private so we can't deal with attribute directly
-            emp.setName("Gg");
-            Console.WriteLine($"new name: {emp.getName()}");
+            //Employee emp = new Employee(10, "Gamal", 22, 7000);
+            //emp.Id = 1000;
+            //Console.WriteLine(emp);
+            ////it's wrong before encapsulation to access attribute directly and
+            ////if we change in thing in struct any instance deal with this struct will be affected 
+            ////we can change it's value 
+            ////we can't add validation on it
+            //// emp.name ->> it will be error because name is private so we can't deal with attribute directly
+            //emp.setName("Gg");
+            //Console.WriteLine($"new name: {emp.getName()}");
 
             #endregion
 
             #region Property
 
             #region automatic property
-            emp.Age = 23;
-            Console.WriteLine($"new age: {emp.Age}");
+
+            //emp.Age = 23;
+            //Console.WriteLine($"new age: {emp.Age}");
+            
             #endregion
 
             #region full property
+
             //Deal with property like attribute but it property it creat a hidden attribute called backfield 
             // it created by compiler in il (assymble code) 
-            Console.WriteLine($"Salary: {emp.Salary}");    
-            #endregion
+            //Console.WriteLine($"Salary: {emp.Salary}");
 
             #endregion
 
+            #endregion
+
+            //Ex02
+            #region PhoneBook 
+
+            #region Setter-Getter
+
+            //phoneBook note=new phoneBook(3);
+            //note.addPerson(0, "Gamal", 123);
+            //note.addPerson(1, "ali", 456);
+            //note.addPerson(2, "mona", 789);
+
+            //Console.WriteLine(note.GetNumber("Gamal")==-1?"not found":note.GetNumber("Gamal"));
+
+            //note.setNumber(999, "Gamal");
+
+            //Console.WriteLine(note.GetNumber("Gamal"));
+
+            #endregion
+
+            #endregion
             #endregion
 
 
